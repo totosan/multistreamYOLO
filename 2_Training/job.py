191ec5ca6f55bc29b96701b74b6306dafcd74c05
Thomas Tomow <toto_san@live.com>
2021-04-29 22:33:31 +0200

    added tiny yolo builds

--- 2_Training/job.py
+++ 2_Training/job.py
@@ -2,67 +2,103 @@
 
 # imports
 import os
+import argparse
+
 from pathlib import Path
 from azureml.core import Workspace, Dataset
 from azureml.core import ScriptRunConfig, Experiment, Environment
 from azureml.widgets import RunDetails
 
-# get workspace
-ws = Workspace.from_config()
-ds = ws.get_default_datastore()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
+    """
+    Command line options
+    """
+    parser.add_argument(
+        "--env_name",
+        type=str,
+        default="TomowsEnv",
+        help="The name of the training environment in Azure ML",
+    )
+    parser.add_argument(
+        "--experiment_name",
+        type=str,
+        default="yolo_tuning",
+        help="The name of the training environment in Azure ML",
+    )
+    parser.add_argument(
+        "--compute_name",
+        type=str,
+        default="gpucluster",
+        help="Name of the used compute system (cluster, instance,...) in Azure ML",
+    )
+    parser.add_argument(
+        "--is_tiny",
+        default=False,
+        action="store_true"
+        help="Create the Yolo tiny version, instead the bigger one",
+    )
+    
+    FLAGS = parser.parse_args()
+    
+    
+    # get workspace
+    ws = Workspace.from_config()
+    ds = ws.get_default_datastore()
 
-# get root of git repo
-prefix = Path(__file__).resolve().parents[1]
+    # get root of git repo
+    prefix = Path(__file__).resolve().parents[1]
 
-#ds.upload(
-#    src_dir="Data/Source_Images",
-#    target_path="YoloTraining/Data/Source_Images",
-#    overwrite=True,
-#)
-dataset = Dataset.File.from_files((ds, "YoloTraining/Data"))
+    # getting images from datastorage
+    dataset = Dataset.File.from_files((ds, "YoloTraining/Data"))
 
-# training script
-script_dir = str(prefix.joinpath("."))
-script_name = "2_Training/Train_YOLO.py"
+    # training script
+    script_dir = str(prefix.joinpath("."))
+    script_name = "2_Training/Train_YOLO.py"
 
-# environment file
-environment_file = str(prefix.joinpath("2_Training/environment.yaml"))
+    # environment file
+    environment_file = str(prefix.joinpath("2_Training/environment.yaml"))
 
-# azure ml settings
-environment_name = "TomowsEnv"
-experiment_name = "yolo_tuning"
-compute_name = "gpucluster"
+    # azure ml settings
+    environment_name = FLAGS.env_name
+    experiment_name = FLAGS.experiment_name
+    compute_name = FLAGS.compute_name
 
-# create environment
-env = Environment.from_conda_specification(environment_name, environment_file)
-# Specify a GPU base image
-env.docker.enabled = True
-env.docker.base_image = 'mcr.microsoft.com/azureml/openmpi4.1.0-cuda11.0.3-cudnn8-ubuntu18.04'
+    # create environment
+    env = Environment.from_conda_specification(environment_name, environment_file)
+    # Specify a GPU base image
+    env.docker.enabled = True
+    env.docker.base_image = 'mcr.microsoft.com/azureml/openmpi4.1.0-cuda11.0.3-cudnn8-ubuntu18.04'
 
-os.makedirs("./outputs", exist_ok=True)
+    os.makedirs("./outputs", exist_ok=True)
 
-# create job config
-src = ScriptRunConfig(
-    source_directory=script_dir,
-    script=script_name,
-    environment=env,
-    compute_target=compute_name,
-    arguments=[
-        "--datastore_path", dataset.as_mount(),
-        #"--epochs",20,
-        "--log_dir", "./outputs"
-    ]
-)
+    # create job config
+    src = ScriptRunConfig(
+        source_directory=script_dir,
+        script=script_name,
+        environment=env,
+        compute_target=compute_name,
+        arguments=[
+            "--datastore_path", dataset.as_mount(),
+            "--epochs",20,
+            "--log_dir", "./outputs",
+            "--is_tiny", FLAGS.is_tiny
+        ]
+    )
 
-# submit job
-run = Experiment(ws, experiment_name).submit(src)
+    # submit job
+    run = Experiment(ws, experiment_name).submit(src)
 
-run.wait_for_completion(show_output=False)
-
-# register model (staged)
-model = run.register_model(model_name='yolov3',
-                        tags={'area': 'Yolo'},
-                        model_path='./outputs')
-print("Registered model:")
-print(model.name, model.id, model.version, sep='\t')
+    run.wait_for_completion(show_output=False)
+    
+    # register models (checkpoint, staged & final together)
+    model_name = "yolov3"
+    if(FLAGS.is_tiny):
+        model_name = model_name + "-tiny"
+        
+    model = run.register_model(model_name=model_name,
+                            tags={'area': 'Yolo'},
+                            model_path='./outputs')
+    print("Registered model:")
+    print(model.name, model.id, model.version, sep='\t')
 
